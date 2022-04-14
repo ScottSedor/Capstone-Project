93080406b630c8b69aaa2ebb86e2927c4b5d73eb
Scott Sedor <scottharrysedor@gmail.com>
2022-04-14 14:23:01 -0400

    merge check

---
+++ vue/src/components/SearchCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search-card">
+      <form v-on:submit.prevent="search" >
+        <label for="search-field" >Search Keyword:</label>
+        <input id="search-field" type="text" placeholder="Keyword" v-model.trim="searchRequest.keyword">
+        <input type="submit" value="Search" >
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchRequest: {
+                keyword: ""
+            },
+        }
+    },
+    methods: {
+        search() {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
