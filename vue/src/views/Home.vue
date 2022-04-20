400aa12d5987273e21618fefd18e6f17e4ad2400
Scott Sedor <scottharrysedor@gmail.com>
2022-04-20 14:08:32 -0400

    CSS styling updates

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -48,20 +48,21 @@ export default {
     background: rgb(240, 240, 240);
     height: 520px;
     box-shadow:
-  0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-  0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-  0 6.5px 5px rgba(0, 0, 0, 0.06),
-  0 12.3px 7.9px rgba(0, 0, 0, 0.072),
-  0 41.8px 23.4px rgba(0, 0, 0, 0.086),
-  0 50px 80px rgba(0, 0, 0, 0.12);
-  border-radius: 10px;
+    0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+    0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+    0 6.5px 5px rgba(0, 0, 0, 0.06),
+    0 12.3px 7.9px rgba(0, 0, 0, 0.072),
+    0 41.8px 23.4px rgba(0, 0, 0, 0.086),
+    0 50px 80px rgba(0, 0, 0, 0.12);
+    border-radius: 10px;
   }
   div.home-buttons {
     display: flex;
+    justify-content: space-evenly;
   }
   button#decks {
-    margin-left: 250px;
-    margin-right: 145px;
+    display: flex;
+    
   }
   div > h1 {
     color: #274e13ff;
