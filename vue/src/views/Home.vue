c11f06be829680ef0c17ef141acc554d167b4b44
Scott Sedor <scottharrysedor@gmail.com>
2022-04-20 13:57:54 -0400

    CSS styling updates

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>Welcome To Stud.io!</h1>
+    <p>Choose an Option to Begin Studying</p>
+
+    <div class="home-buttons">
     <router-link :to="{name: 'decks'}">
       <button id="decks">
-        <img class="studio-img" data-toggle="popover" title="View All Decks" src="..\assets\blank-white-studio.jpg" alt="blank white study">
+        <img class="studio-img" data-toggle="popover" title="View All Decks" src="..\assets\blankstudio.jpg" alt="blank white study">
       </button>
-    </router-link>  
+    </router-link>
+    
     <router-link :to="{name: 'study-session-home'}">
       <button id="start-study-button">
         <img class="start-study-img" data-toggle="popover" title="Start Study Session" src="..\assets\play-button-icon.png" alt="play button icon">
       </button>
     </router-link>
+    </div>
+    
+    <div class="option-text">
+      <p class="">Look at your flashcard decks</p>
+      <p>Start a study session now</p> 
+    
+    </div>
   </div>
 </template>
 
@@ -22,10 +32,42 @@ export default {
 </script>
 
 <style>
+  div.option-text {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  div.home > h1 {
+    margin-top: 20px;
+    text-shadow: rgba(110, 110, 110, 0.377) 5px 0 10px;
+  }
+  button#start-study-button > img.start-study-img {
+    height: 300px;
+    width: 300px;
+  }
+  div.home {
+    background: rgb(240, 240, 240);
+    height: 520px;
+    box-shadow:
+  0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+  0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+  0 6.5px 5px rgba(0, 0, 0, 0.06),
+  0 12.3px 7.9px rgba(0, 0, 0, 0.072),
+  0 41.8px 23.4px rgba(0, 0, 0, 0.086),
+  0 50px 80px rgba(0, 0, 0, 0.12);
+  border-radius: 10px;
+  }
+  div.home-buttons {
+    display: flex;
+  }
+  button#decks {
+    margin-left: 250px;
+    margin-right: 145px;
+  }
   div > h1 {
     color: #274e13ff;
   }
   div > p {
+    text-align: center;
     color: #274e13ff;
   }
   div.deck-list {
@@ -33,5 +75,9 @@ export default {
   }
   div.study-session-home {
     color:  #274e13ff;
+  }
+  button {
+    border: none;
+    background: none;
   }
 </style>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -38,9 +38,17 @@
       <button class="submit" type="submit">Sign in</button>
       <router-link :to="{ name: 'register' }">Need an account?</router-link>
       </div>
+
+      <div id="before">
+      <p>Before Stud.io</p>
       <img class="angry-img" src="..\assets\angry-learner.png" alt="frustrated child studying">
+      </div>
+
+      <div id="after">
+      <p>One Week Of Stud.io</p>
       <img class="happy-img" src="..\assets\happy-learner.png" alt="happy woman learning">
-      
+      </div>
+
     </form>
   </div>
 </template>
@@ -84,36 +92,48 @@ export default {
 </script>
 
 <style>
+
+div#before > p, div#after > p {
+  text-align: center;
+}
+
+div#before {
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+}
+div#after {
+  margin-left: 50px;
+  display: flex;
+  flex-direction: column;
+}
 div.login-fields {
   display: flex;
   flex-direction: column;
   margin: 20px;
+  margin-right: 285px;
 }
 img.angry-img {
-  margin-top: 20px;
-  margin-left: 10px;
-  height: 300px;
-  width: 300px;
+  height: 280px;
+  width: 280px;
 }
 img.happy-img {
-  margin-top: 20px;
-  margin-left: 10px;
-  height: 300px;
-  width: 450px
+  height: 280px;
+  width: 420px
 }
-
 form.form-signin {
   display: flex;
   height: 350px;
   background: rgb(240, 240, 240);
   margin-top: 30px;
-   box-shadow:
+  box-shadow:
   0 2.8px 2.2px rgba(0, 0, 0, 0.034),
   0 6.7px 5.3px rgba(0, 0, 0, 0.048),
   0 6.5px 5px rgba(0, 0, 0, 0.06),
   0 12.3px 7.9px rgba(0, 0, 0, 0.072),
   0 41.8px 23.4px rgba(0, 0, 0, 0.086),
   0 50px 80px rgba(0, 0, 0, 0.12);
+  border-radius: 5px;
 }
 form.form-register {
   background: rgb(240, 240, 240);
